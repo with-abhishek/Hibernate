5fef40044344b46e95e4db2a229b291720db9765
withabhi <asp9514@gmail.com>
2024-01-03 23:46:20 +0530

    Initial commit

---
+++ src/main/java/hibernate/client/App.java
@@ -0,0 +1,39 @@
+package hibernate.client;
+import hibernate.entity.Employee;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+public class App {
+
+	public static void main(String[] args) {
+		
+		Employee em = new Employee();
+		
+//		em.setId(9);w
+		em.setName("Abhinav");
+		em.setGender("Male");
+		em.setSalary(41000);
+		
+		ServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		Metadata data = new MetadataSources(ssr).getMetadataBuilder().build();
+		
+//		Configuration cfg = new Configuration().configure();
+		SessionFactory sf = data.buildSessionFactory();
+		Session ss = sf.openSession();
+		Transaction tn = ss.beginTransaction();
+		
+		ss.save(em);
+		
+//		em = ss.load(Employee.class, 1); 
+//		System.out.println(em);
+		tn.commit();
+		
+	}
+
+}
---
+++ src/main/java/hibernate/entity/Employee.java
@@ -0,0 +1,53 @@
+package hibernate.entity;
+
+public class Employee {
+
+	
+	private  int id;
+	private String name;
+	private String gender;
+
+	private int salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
+	}
+	
+	
+
+	
+
+}
