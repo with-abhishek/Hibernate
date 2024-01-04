7810871852f3ca5f84de75e5aaa8191627711deb
withabhi <asp9514@gmail.com>
2024-01-04 23:31:39 +0530

    Add annotation for Employee class

--- src/main/java/hibernate/entity/Employee.java
+++ src/main/java/hibernate/entity/Employee.java
@@ -1,5 +1,14 @@
 package hibernate.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
 public class Employee {
 
 	
@@ -9,42 +18,6 @@ public class Employee {
 
 	private int salary;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public int getSalary() {
-		return salary;
-	}
-
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
-	}
 	
 	
 
