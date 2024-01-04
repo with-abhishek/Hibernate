71d23bef200b41888e36d07534cdb9d47bc6a628
withabhi <asp9514@gmail.com>
2024-01-04 23:29:49 +0530

    Add Lombok dependency

--- src/main/java/hibernate/client/App.java
+++ src/main/java/hibernate/client/App.java
@@ -7,7 +7,6 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 public class App {
 
